60d3b3b87a47dcb261b2a4ac8613e4afaa160690
Snehil Shubham <snehil1693@gmail.com>
2025-01-02 13:20:42 +0530

    Updating the print file

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,9 @@ echo Hello World
 #For new line or to print multiple lines
 echo -e "Hello World \n\t"
 echo -e "How are you guys"
+
+#for printing things in different colours
+
+echo -e "\e[31mMESSAGE IN RED"
+echo -e "\e[33mMESSAGE IN YELLOW"
+
