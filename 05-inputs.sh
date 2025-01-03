3b859e28621e700cf8a17ca3a33e36641c10980c
Snehil Shubham <snehil1693@gmail.com>
2025-01-03 18:07:23 +0530

    Making changes in read file

---
+++ 05-inputs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+#if we want to access the script name itself
+echo $0
+
+#$1 is the first argument and $n is the nth argument
+echo $1
+
+#pull all the parsed value
+echo $*
+echo $@
+
