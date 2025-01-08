9d8881c0b643eb293633cb1b660947c727f78da7
Snehil Shubham <snehil1693@gmail.com>
2025-01-08 14:06:15 +0530

    Make frontend changes

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -11,7 +11,7 @@ else
   echo fail
 fi
 
-echo -n -e "Enabling Nginx\t\t..."
+echo -n -e "Enabling Nginx\t\t\t..."
 systemctl enable nginx &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
@@ -19,7 +19,7 @@ else
   echo fail
 fi
 
-echo -n -e "Starting Nginx\t\t..."
+echo -n -e "Starting Nginx\t\t\t..."
 systemctl start nginx &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
