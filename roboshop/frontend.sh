e992b65b1198f8e96782acba891349de4888f5de
Snehil Shubham <snehil1693@gmail.com>
2025-01-08 17:51:16 +0530

    Make frontend changes

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,14 +2,30 @@
 
 source common.sh
 
-PRINT "Installing Nginx"
+PRINT "Installing Nginx\t"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Enabling Nginx\t"
+PRINT "Download Frontend\t"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove Old Htdocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config\t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
+
+PRINT "Enabling Nginx\t\t"
 systemctl enable nginx &>>$LOG
 STAT_CHECK $?
 
-PRINT "Starting Nginx\t"
-systemctl start nginx &>>$LOG
+PRINT "Starting Nginx\t\t"
+systemctl restart nginx &>>$LOG
 STAT_CHECK $?
