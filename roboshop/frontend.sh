1e57d088999b1c63fc0ace68127a9b8de12d5e8a
Snehil Shubham <snehil1693@gmail.com>
2025-01-08 17:23:43 +0530

    Make frontend changes

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-LOG=/tmp/roboshop.log
-rm -rf $LOG
+source common.sh
 
-echo -n -e "Installing Nginx\t\t..."
+PRINT "Installing Nginx"
 yum install nginx -y &>>$LOG
+STAT_CHECK $?
 
-
-echo -n -e "Enabling Nginx\t\t\t..."
+echo -n -e "Enabling Nginx\t"
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
 
-echo -n -e "Starting Nginx\t\t\t..."
+echo -n -e "Starting Nginx\t"
 systemctl start nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
