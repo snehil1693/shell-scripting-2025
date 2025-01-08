7c4fe96a0e919170e5790a34eb63848016a37796
Snehil Shubham <snehil1693@gmail.com>
2025-01-08 17:21:54 +0530

    Make frontend changes

---
+++ roboshop/common.sh
@@ -0,0 +1,24 @@
+#!/bain/bash
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ] ; then
+  echo -e "\e[31mYou should be a root user / sudo user to run this script\e[0m"
+  exit 2
+fi
+
+LOG=/tmp/roboshop.log
+rm -rd $LOG
+
+STAT_CHECK(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+  ## so if the code fail it should not move forward
+    exit 1
+  fi
+}
+
+PRINT(){
+  echo -n -e "$1\t\t"
+}
--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -5,11 +5,7 @@ rm -rf $LOG
 
 echo -n -e "Installing Nginx\t\t..."
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-fi
+
 
 echo -n -e "Enabling Nginx\t\t\t..."
 systemctl enable nginx &>>$LOG
@@ -17,6 +13,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
 else
   echo -e "\e[31m fail\e[0m"
+  exit 1
 fi
 
 echo -n -e "Starting Nginx\t\t\t..."
@@ -25,4 +22,5 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
 else
   echo -e "\e[31m fail\e[0m"
+  exit 1
 fi
