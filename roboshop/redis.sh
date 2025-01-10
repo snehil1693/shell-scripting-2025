ba3c32a8ea0c6a4fe208e2ca78240973face824b
Snehil Shubham <snehil1693@gmail.com>
2025-01-10 14:17:59 +0530

    redis changes

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source common.sh
 
-PRINT "Install Redis Repos"
+PRINT "Install Redis Repos\t"
 yum install epel-release  -y &>>$LOG && yum install http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Install Redis\t"
+PRINT "Install Redis\t\t"
 yum-config-manager --enable remi &>>$LOG && yum install redis -y &>>$LOG
 STAT_CHECK $?
 
@@ -13,6 +13,6 @@ PRINT "Update Redis Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 STAT_CHECK $?
 
-PRINT "Start Redis Service"
+PRINT "Start Redis Service\t"
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 STAT_CHECK $?
